e512fa8d4620b593a9d26ae71ffbdcf0b715ab83
Giacomo Guiduzzi <giacomo.guiduzzi@unimore.it>
2023-11-20 11:02:54 +0100

    Added some checks to avoid crashing due to uncomplete logs. Done a little clean-up. Fixed a bug about merging results.

--- parse_experiments.py
+++ parse_experiments.py
@@ -71,26 +71,27 @@ def parse_ists(file):
             model_type = info.split('=')[-1]
 
     with open(file, 'r') as log_file:
+        idx_string = f"{dataset_name}_{subset}_nan{nan_num}_nf{num_fut}_{model_type}"
+        df.loc[idx_string] = [np.nan] * len(df.columns)
+        loss_df.loc[idx_string] = [np.nan] * len(loss_df.columns)
+
         for line in log_file:
             if 'test_r2' in line and 'train_r2' in line:
                 metrics = eval(line)
 
-                df.loc[f"{dataset_name}_{subset}_nan{nan_num}_nf{num_fut}_{model_type}", ['Train_R2', 'Test_R2']] = \
-                    [metrics['train_r2'], metrics['test_r2']]
-                df.loc[f"{dataset_name}_{subset}_nan{nan_num}_nf{num_fut}_{model_type}", ['Train_MSE', 'Test_MSE']] = \
-                    [metrics['train_mse'], metrics['test_mse']]
-                df.loc[f"{dataset_name}_{subset}_nan{nan_num}_nf{num_fut}_{model_type}", ['Train_MAE', 'Test_MAE']] = \
-                    [metrics['train_mae'], metrics['test_mae']]
+                df.loc[idx_string, ['Train_R2', 'Test_R2']] = [metrics['train_r2'], metrics['test_r2']]
+                df.loc[idx_string, ['Train_MSE', 'Test_MSE']] = [metrics['train_mse'], metrics['test_mse']]
+                df.loc[idx_string, ['Train_MAE', 'Test_MAE']] = [metrics['train_mae'], metrics['test_mae']]
 
                 for epoch, (loss, val_loss) in enumerate(zip(metrics['loss'], metrics['val_loss'])):
-                    loss_df.loc[f"{dataset_name}_{subset}_nan{nan_num}_nf{num_fut}_{model_type}",
-                                [f'Loss_{epoch}', f'Val_Loss_{epoch}']] = [loss, val_loss]
+                    loss_df.loc[idx_string, [f'Loss_{epoch}', f'Val_Loss_{epoch}']] = [loss, val_loss]
 
     # maybe superfluous
     df_dict['ISTS'] = df
 
 
 def parse_model(model, file):
+    nan = -999
     df = df_dict[model]
 
     dataset_name = file.split(file_start[model])[-1]
@@ -103,97 +104,111 @@ def parse_model(model, file):
         metrics = log_file.readlines()[-20:]
 
         if not metrics:
+            df.loc[dataset_name] = [np.nan] * len(df.columns)
             return  # file is not complete or empty
 
         if 'ValueError' in metrics[-1]:  # if error save NaN
             for mode in ['Train', 'Valid', 'Test']:
-                df.loc[dataset_name, [f'{mode}_R2', f'{mode}_MSE', f'{mode}_MAE']] = np.nan
+                df.loc[dataset_name, [f'{mode}_R2', f'{mode}_MSE', f'{mode}_MAE']] = nan
 
         else:  # parse the file with the correct modality per model
             if model == 'GRU-D':
-                if 'Performance metrics:' in metrics[-6]:
-                    metrics = metrics[-5:-2]
+                try:
+                    if 'Performance metrics:' in metrics[-6]:
+                        metrics = metrics[-5:-2]
 
-                    for line, metric in zip(metrics, ['R2', 'MAE', 'MSE']):
-                        # train, valid, test
-                        line = line.strip().split(f'{metric} score: ')[-1]
-                        try:
-                            metrics = eval(line)
+                        for line, metric in zip(metrics, ['R2', 'MAE', 'MSE']):
+                            # train, valid, test
+                            line = line.strip().split(f'{metric} score: ')[-1]
+                            try:
+                                metrics = eval(line)
 
-                        except SyntaxError as se:
-                            if not args.ignore_incomplete:
-                                raise SyntaxError(f'Could not eval {file}: {se}.')
-                            continue
+                            except SyntaxError as se:
+                                if not args.ignore_incomplete:
+                                    raise SyntaxError(f'Could not eval {file}: {se}.')
+                                continue
 
-                        if isinstance(metrics, list):
-                            df.loc[dataset_name,
-                                   [f'Train_{metric}', f'Valid_{metric}', f'Test_{metric}']] = metrics
+                            if isinstance(metrics, list):
+                                df.loc[dataset_name,
+                                       [f'Train_{metric}', f'Valid_{metric}', f'Test_{metric}']] = metrics
 
-                        else:
-                            raise ValueError(f'Could not parse {file}: unknown metric format.')
+                            else:
+                                raise ValueError(f'Could not parse {file}: unknown metric format.')
 
-                else:
-                    if not args.ignore_incomplete:
-                        raise ValueError(f'Could not parse {file}: unknown structure.')
+                    else:
+                        if not args.ignore_incomplete:
+                            raise ValueError(f'Could not parse {file}: unknown structure.')
+                except IndexError:
+                    print(f'Could not parse {file}: unknown structure.')
+                    df.loc[dataset_name] = [np.nan] * len(df.columns)
 
             elif model == 'CRU':
-                metrics = metrics[-3:]
-                if 'Train MAE' in metrics[-1]:
+                try:
+                    metrics = metrics[-3:]
+                    if 'Train MAE' in metrics[-1]:
 
-                    for line, metric in zip(metrics, ['R2', 'MSE', 'MAE']):
-                        train_valid, test = line.split(f'Test RMSE:' if 'RMSE' in line
-                                                       else f'Test {metric}:')
+                        for line, metric in zip(metrics, ['R2', 'MSE', 'MAE']):
+                            train_valid, test = line.split(f'Test RMSE:' if 'RMSE' in line
+                                                           else f'Test {metric}:')
 
-                        train_valid = train_valid.split(', ')[0].split(f'Train RMSE: ' if 'RMSE' in line
-                                                                       else f'Train {metric}: ')[-1]
-                        train_valid = float(train_valid)
-                        test = float(test.strip())
+                            train_valid = train_valid.split(', ')[0].split(f'Train RMSE: ' if 'RMSE' in line
+                                                                           else f'Train {metric}: ')[-1]
+                            train_valid = float(train_valid)
+                            test = float(test.strip())
 
-                        df.loc[dataset_name,
-                               [f'Train_{metric}', f'Valid_{metric}']] = train_valid
-                        df.loc[dataset_name, f'Test_{metric}'] = test
-                else:
-                    if not args.ignore_incomplete:
-                        raise ValueError(f'Could not parse {file}: unknown structure.')
+                            df.loc[dataset_name,
+                                   [f'Train_{metric}', f'Valid_{metric}']] = train_valid
+                            df.loc[dataset_name, f'Test_{metric}'] = test
+                    else:
+                        if not args.ignore_incomplete:
+                            raise ValueError(f'Could not parse {file}: unknown structure.')
+                except IndexError:
+                    print(f'Could not parse {file}: unknown structure.')
+                    df.loc[dataset_name] = [np.nan] * len(df.columns)
 
             elif model == 'mTAN':
-                metrics = metrics[-3].split(', ')
+                try:
+                    metrics = metrics[-3].split(', ')
 
-                if len(metrics) > 5 and 'train_mse' in metrics[4]:
-                    train_mse = metrics[4].split('train_mse: ')[-1]
-                    train_mae = metrics[5].split('train_mae: ')[-1]
-                    train_r2 = metrics[6].split('train_r2: ')[-1]
+                    if len(metrics) > 5 and 'train_mse' in metrics[4]:
+                        train_mse = metrics[4].split('train_mse: ')[-1]
+                        train_mae = metrics[5].split('train_mae: ')[-1]
+                        train_r2 = metrics[6].split('train_r2: ')[-1]
 
-                    train_mse = float(train_mse) if train_mse != 'nan' else np.nan
-                    train_mae = float(train_mae) if train_mae != 'nan' else np.nan
-                    train_r2 = float(train_r2) if train_r2 != 'nan' else np.nan
+                        train_mse = float(train_mse) if train_mse != 'nan' else nan
+                        train_mae = float(train_mae) if train_mae != 'nan' else nan
+                        train_r2 = float(train_r2) if train_r2 != 'nan' else nan
 
-                    df.loc[dataset_name, ['Train_MSE', 'Train_MAE', 'Train_R2']] = \
-                        [train_mse, train_mae, train_r2]
-                    val_mse = metrics[9].split('val_mse: ')[-1]
-                    val_mae = metrics[10].split('val_mae: ')[-1]
-                    val_r2 = metrics[11].split('val_r2: ')[-1]
+                        df.loc[dataset_name, ['Train_MSE', 'Train_MAE', 'Train_R2']] = \
+                            [train_mse, train_mae, train_r2]
+                        val_mse = metrics[9].split('val_mse: ')[-1]
+                        val_mae = metrics[10].split('val_mae: ')[-1]
+                        val_r2 = metrics[11].split('val_r2: ')[-1]
 
-                    val_mse = float(val_mse) if val_mse != 'nan' else np.nan
-                    val_mae = float(val_mae) if val_mae != 'nan' else np.nan
-                    val_r2 = float(val_r2) if val_r2 != 'nan' else np.nan
+                        val_mse = float(val_mse) if val_mse != 'nan' else nan
+                        val_mae = float(val_mae) if val_mae != 'nan' else nan
+                        val_r2 = float(val_r2) if val_r2 != 'nan' else nan
 
-                    df.loc[dataset_name, ['Valid_MSE', 'Valid_MAE', 'Valid_R2']] = \
-                        [val_mse, val_mae, val_r2]
-                    test_mse = metrics[13].split('test_mse: ')[-1]
-                    test_mae = metrics[14].split('test_mae: ')[-1]
-                    test_r2 = metrics[15].split('test_r2: ')[-1].strip()
+                        df.loc[dataset_name, ['Valid_MSE', 'Valid_MAE', 'Valid_R2']] = \
+                            [val_mse, val_mae, val_r2]
+                        test_mse = metrics[13].split('test_mse: ')[-1]
+                        test_mae = metrics[14].split('test_mae: ')[-1]
+                        test_r2 = metrics[15].split('test_r2: ')[-1].strip()
 
-                    test_mse = float(test_mse) if test_mse != 'nan' else np.nan
-                    test_mae = float(test_mae) if test_mae != 'nan' else np.nan
-                    test_r2 = float(test_r2) if test_r2 != 'nan' else np.nan
+                        test_mse = float(test_mse) if test_mse != 'nan' else nan
+                        test_mae = float(test_mae) if test_mae != 'nan' else nan
+                        test_r2 = float(test_r2) if test_r2 != 'nan' else nan
 
-                    df.loc[dataset_name, ['Test_MSE', 'Test_MAE', 'Test_R2']] = \
-                        [test_mse, test_mae, test_r2]
+                        df.loc[dataset_name, ['Test_MSE', 'Test_MAE', 'Test_R2']] = \
+                            [test_mse, test_mae, test_r2]
 
-                else:
-                    if not args.ignore_incomplete:
-                        raise ValueError(f'Could not parse {file}: unknown structure.')
+                    else:
+                        if not args.ignore_incomplete:
+                            raise ValueError(f'Could not parse {file}: unknown structure.')
+
+                except IndexError:
+                    print(f'Could not parse {file}: unknown structure.')
+                    df.loc[dataset_name] = [np.nan] * len(df.columns)
 
             else:
                 raise ValueError(f'Unknown model {model}.')
@@ -286,10 +301,10 @@ def merge():
         ists_results = pd.read_csv(os.path.join(ists_results_path, csv_file), index_col=0, header=[0, 1])
         others_results = pd.read_csv(csv_file, index_col=0, header=[0, 1])
 
-        others_results.loc[:, ('ISTS', 0.0)] = ists_results.loc[:, ('ISTS', 0.0)]
-        others_results.loc[:, ('ISTS', 0.2)] = ists_results.loc[:, ('ISTS', 0.2)]
-        others_results.loc[:, ('ISTS', 0.5)] = ists_results.loc[:, ('ISTS', 0.5)]
-        others_results.loc[:, ('ISTS', 0.8)] = ists_results.loc[:, ('ISTS', 0.8)]
+        others_results.loc[:, ('ISTS', '0.0')] = ists_results.loc[:, ('ISTS', '0.0')]
+        others_results.loc[:, ('ISTS', '0.2')] = ists_results.loc[:, ('ISTS', '0.2')]
+        others_results.loc[:, ('ISTS', '0.5')] = ists_results.loc[:, ('ISTS', '0.5')]
+        others_results.loc[:, ('ISTS', '0.8')] = ists_results.loc[:, ('ISTS', '0.8')]
 
         others_results.to_csv(csv_file)
 
